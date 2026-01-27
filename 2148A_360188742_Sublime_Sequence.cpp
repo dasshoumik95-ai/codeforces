d586578c37fcbce14c1ecceb09f4a049813897b4
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-27 16:49:29 +0530

    Accepted: 2148A - Sublime Sequence (Submission 360188742)

---
+++ 2148A_360188742_Sublime_Sequence.cpp
@@ -0,0 +1,18 @@
+// Codeforces Problem: 2148A - Sublime Sequence
+// Submission ID: 360188742
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int t; cin >> t;
+    while(t--){
+    int x,n;
+    cin >> x >> n;
+    if(n%2==0)
+    cout << 0 <<endl;
+    else 
+    cout << x << endl;
+    }
+}
