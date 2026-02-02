0b134182f0ad5ebdcfbb8ec566c50412ca19f2f8
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-02-02 16:55:19 +0530

    Accepted: 1834A - Unit Array

---
+++ 1834A_Unit_Array.cpp
@@ -0,0 +1,34 @@
+// Codeforces Problem: 1834A - Unit Array
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t; 
+    cin >> t;
+     while(t--)
+    {
+        int n; cin >> n;
+        int cnt1=0 , cnt2=0;
+        vector<int> a(n);
+        for(int &i : a)
+        {
+            cin >> i;
+            if(i==-1)
+                cnt1++;
+            else
+                cnt2++;
+        }
+        int opDone =0;
+        while(cnt1 % 2 != 0 || cnt2 < cnt1)
+        {
+                        cnt2++;
+            cnt1--;
+            opDone++;
+                                }
+        cout << opDone << endl;    
+            }
+}
