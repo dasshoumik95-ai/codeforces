a769230de3cab2d42f53b8800ca157e2d783522e
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-02-05 18:02:39 +0530

    Accepted: 2189A - Table with Numbers (Submission 361598692)

---
+++ 2189A_361598692_Table_with_Numbers.cpp
@@ -0,0 +1,29 @@
+// Codeforces Problem: 2189A - Table with Numbers
+// Submission ID: 361598692
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t; 
+    cin >> t;
+     while(t--)
+    {
+        int n, h, l;
+        cin >> n >> h >> l;
+         if(h>l)
+            swap(h,l);
+                int cnth=0 , cntl=0 ;
+        vector<int>a(n);
+        for(int i =0 ; i< n ; i++)
+        {
+            cin >> a[i];
+            if(a[i]<=h)    cnth++;
+             if(a[i]<=l)    cntl++;
+         }
+        cout << min(cnth,cntl/2) << endl;
+    }
+}
