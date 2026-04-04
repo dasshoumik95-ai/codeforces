a8193b8d0181eab95e670a9c235b213f06332fa5
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-04-04 14:43:18 +0530

    Accepted: 2218A - The 67th Integer Problem (Submission 369628826)

---
+++ 2218A_369628826_The_67th_Integer_Problem.cpp
@@ -0,0 +1,22 @@
+// Codeforces Problem: 2218A - The 67th Integer Problem
+// Submission ID: 369628826
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std ;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+     int t ; cin >> t ;
+    while(t--)
+    {
+        int x ; cin >> x ; 
+         if(x==67)
+            cout << 67 << endl;
+         else
+            cout << x + 1 << endl;
+     }
+}
+ // -67 x 67
+// -66 y 67
