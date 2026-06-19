ba703fde1d249934b5f731e46696106310bbf49a
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-19 18:31:32 +0530

    Accepted: 1734C - Removing Smallest Multiples (Submission 379532036)

---
+++ 1734C_379532036_Removing_Smallest_Multiples.cpp
@@ -0,0 +1,79 @@
+// Codeforces Problem: 1734C - Removing Smallest Multiples
+// Submission ID: 379532036
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: Removing_Smallest_Multiples
+// Rating: Rating
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+;
+void reverse_seive(vector<bool> &a , int n , vector<int> &cost )
+{
+    for1n(i,n)
+    {
+        for(int j = i ; j <= n ; j+=i) 
+        {
+            if(a[j]) break ;
+            cost[j] = i ;
+        }
+    }
+}
+void solve() {
+    int n ; cin >> n ;
+    string s ; cin >> s ;
+     vector<bool> a(n+1) ;
+    vector<int> cost(n+1,0) ;
+    for1(i,n)
+        a[i] = (s[i-1] == '1' ) ;
+     ll sol = 0 ;
+    reverse_seive(a,n,cost) ;
+    for1(i,n)
+    {
+        if(!a[i])
+            sol += cost[i] ;
+    }
+     cout << sol << '\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
