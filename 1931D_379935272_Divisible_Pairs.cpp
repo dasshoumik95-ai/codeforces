9ad031b9be26ec868faa3bb3ff9a3fa0c373a6ef
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-23 16:56:38 +0530

    Accepted: 1931D - Divisible Pairs (Submission 379935272)

---
+++ 1931D_379935272_Divisible_Pairs.cpp
@@ -0,0 +1,91 @@
+// Codeforces Problem: 1931D - Divisible Pairs
+// Submission ID: 379935272
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: D_Divisible_Pairs
+// Rating: Rating
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ void solve() {
+    int n , x , y ;
+    cin >> n >> x >> y ;
+    vector<int> a(n) ;
+    vecInput(a)
+     // map<int,int> cntmodx , cntmody ;
+    map<pair<int, int>, int> freq;
+    // doublepass hash
+    // for0(i,n)
+    // {
+    //     int modx = a[i]%x ,
+    //     mody = a[i]%y ;
+     //     freq[{modx,mody}]++ ;
+    // }
+    // int sol = 0 ;
+    // for0(i,n)
+    // {
+    //     int modx = a[i]%x ,
+    //     mody = a[i]%y ;
+     //     int reqx = (x - modx)%x ,
+    //     reqy = mody ;
+     //     if (freq.count({reqx , reqy})) 
+    //         sol += freq[{reqx , reqy}];
+      //     if(reqx == modx && reqy == mody)
+    //         sol-- ;
+            // }
+     // single pass 
+     ll sol = 0 ;
+    for0(i,n)
+    {
+        int modx = a[i]%x ,
+        mody = a[i]%y ;
+         int reqx = (x - modx)%x ,
+        reqy = mody ;
+         if (freq.count({reqx , reqy})) 
+            sol += (ll)freq[{reqx , reqy}];
+          freq[{modx,mody}]++ ;
+    }
+    cout << sol << '\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
