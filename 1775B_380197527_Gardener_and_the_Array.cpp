c1749906397426f07e2b2e992187132036aa59a8
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-26 08:43:14 +0530

    Accepted: 1775B - Gardener and the Array (Submission 380197527)

---
+++ 1775B_380197527_Gardener_and_the_Array.cpp
@@ -0,0 +1,83 @@
+// Codeforces Problem: 1775B - Gardener and the Array
+// Submission ID: 380197527
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: B_Gardener_and_the_Array
+// Rating: Rating
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ void solve() {
+    int n ; cin >> n ;
+        map<int,int> cntbit ;
+    vector<vector<int>> c(n,vector<int>()) ;
+     for0(i,n)
+    {
+        int x ; cin >> x ;
+        // c[i].assign(x) ;
+         for0(j,x)
+        {
+            int y ; cin >> y ;
+            c[i].pb(y) ;
+            cntbit[y]++ ;
+        }
+    }
+    bool ok = false ;
+    for(auto &coloums : c)
+    {
+        bool exclude = true ;
+        for(auto &rows : coloums)
+            if(cntbit[rows] < 2) {
+                exclude = false ;
+                break ;
+            }
+         if(exclude)
+            ok = true ;
+    }
+    if(ok)
+        YES
+    else
+        NO
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
