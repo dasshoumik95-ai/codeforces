72b4e23a36b70a742eb74397fcd7081a27d032a7
Shoumik Das <dasshoumik95@gmail.com>
2026-06-28 20:47:35 +0530

    Add Unit Array [A] from Codeforces

---
+++ 1834/A - Unit Array/solution.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t; 
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n; cin >> n;
+        int cnt1=0 , cnt2=0;
+        vector<int> a(n);
+        for(int &i : a)
+        {
+            cin >> i;
+            if(i==-1)
+                cnt1++;
+            else
+                cnt2++;
+        }
+        int opDone =0;
+        while(cnt1 % 2 != 0 || cnt2 < cnt1)
+        {
+            
+            cnt2++;
+            cnt1--;
+            opDone++;
+            
+            
+        }
+        cout << opDone << endl;    
+        
+    }
+}
